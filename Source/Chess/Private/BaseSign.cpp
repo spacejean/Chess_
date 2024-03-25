74f20797a0c9085e16664c33600fedb134ce26eb
spacejean <160678931+spacejean@users.noreply.github.com>
2024-03-25 15:43:00 +0100

    FirstCommit

---
+++ Source/Chess/Private/BaseSign.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BaseSign.h"
+#include "GameField.h"
+#include "Chess_GameMode.h"
+
+// Sets default values
+ABaseSign::ABaseSign()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+// Called when the game starts or when spawned
+void ABaseSign::BeginPlay()
+{
+	Super::BeginPlay();
+	AChess_GameMode* GameMode = Cast<AChess_GameMode>(GetWorld()->GetAuthGameMode());
+	GameMode->GField->OnResetEvent.AddDynamic(this, &ABaseSign::SelfDestroy);
+}
+
+// Called every frame
+void ABaseSign::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ABaseSign::SelfDestroy()
+{
+	Destroy();
+}
+
+
+
---
+++ Source/Chess/Public/Tile.h
@@ -0,0 +1,68 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Tile.generated.h"
+
+UENUM()
+enum class ETileStatus : uint8
+{
+	EMPTY     UMETA(DisplayName = "Empty"),
+	OCCUPIED      UMETA(DisplayName = "Occupied"),
+};
+
+UCLASS()
+class CHESS_API ATile : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ATile();
+
+	// set the player owner and the status of a tile
+	void SetTileStatus(const int32 TileOwner, const ETileStatus TileStatus);
+
+	// get the tile status
+	ETileStatus GetTileStatus();
+
+	// get the tile owner
+	int32 GetOwner();
+
+	// set the (x, y) position
+	void SetGridPosition(const double InX, const double InY);
+
+	// get the (x, y) position
+	FVector2D GetGridPosition();
+
+	// Set material function
+	void SetMaterial(class UMaterialInterface* Material);
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	USceneComponent* Scene;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UStaticMeshComponent* StaticMeshComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	ETileStatus Status;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	int32 PlayerOwner;
+
+	// (x, y) position of the tile
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	FVector2D TileGridPosition;
+private:
+	UMaterialInterface* CurrentMaterial;
+	//public:	
+	//	// Called every frame
+	//	virtual void Tick(float DeltaTime) override;
+
+};
